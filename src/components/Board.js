672c6831301524c51066d49e4bf0f9f7f8f88cb4
Mauro Souto <maurosouto27@gmail.com>
2024-05-21 12:53:15 -0300

    project updated

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,80 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import Card from './Card.js'
-import './Board.css'
-import IMAGES from './Images.js'
+import React, { useState, useEffect } from "react";
+import Card from "./Card.js";
+import "./Board.css";
+import IMAGES from "./Images.js";
 
 export default function Board() {
+  const [cards, setCards] = useState([...IMAGES, ...IMAGES]);
 
-    // eslint-disable-next-line
-    const [cards, setCards] = useState([...IMAGES, ...IMAGES])
-    // eslint-disable-next-line
-    const [orders, setOrders] = useState(()=>{
-        const o = []
-        cards.forEach(()=> o.push(Math.floor(Math.random()*cards.length)))
-        return o
-    })
+  const [orders, setOrders] = useState(() => {
+    const o = [];
+    cards.forEach(() => o.push(Math.floor(Math.random() * cards.length)));
+    return o;
+  });
 
-    const [firstCard, setFirstCard] = useState(null)
-    const [secondCard, setSecondCard] = useState(null)
+  const [firstCard, setFirstCard] = useState(null);
+  const [secondCard, setSecondCard] = useState(null);
 
-    const [freezeBoard, setFreezeBoard] = useState(false)
+  const [freezeBoard, setFreezeBoard] = useState(false);
 
-    
-    useEffect(() => {
-       
-        checkMatch()
-       // eslint-disable-next-line
-    }, [secondCard])
-    
+  useEffect(() => {
+    checkMatch();
+  }, [secondCard]);
 
-
-    const handleCardClick = (isFlipped, name, callback) => {
-        if(isFlipped) return
-        callback(false)
-        if(!firstCard){
-            return setFirstCard({name, callback})
-        }
-        if(!secondCard){
-            setSecondCard({name, callback})
-        }
+  const handleCardClick = (isFlipped, name, callback) => {
+    if (isFlipped) return;
+    callback(false);
+    if (!firstCard) {
+      return setFirstCard({ name, callback });
     }
-
-    const checkMatch = () => {
-        if(!secondCard) return
-
-        setFreezeBoard(true)
-
-        if(firstCard.name===secondCard.name){
-            console.log('matchearon')
-            setFirstCard(null)
-            setSecondCard(null)
-            setFreezeBoard(false)
-        }
-        else{
-            setTimeout( () => {
-                firstCard.callback(true)
-                secondCard.callback(true)
-                setFirstCard(null)
-                setSecondCard(null)
-                setFreezeBoard(false)
-            }, 2000)
-        }
-        
+    if (!secondCard) {
+      setSecondCard({ name, callback });
     }
-    
+  };
 
-    return (
-        <section id="board">
-            {cards.map( (i,idx) => {
+  const checkMatch = () => {
+    if (!secondCard) return;
 
-                return <Card 
-                        onClick={handleCardClick}
-                        boardIsFreeze={freezeBoard}
-                        key={idx} 
-                        image={i}
-                        order={orders[idx]}
-                        />
-            })}
-        </section>
-    )
+    setFreezeBoard(true);
+
+    if (firstCard.name === secondCard.name) {
+      console.log("matchearon");
+      setFirstCard(null);
+      setSecondCard(null);
+      setFreezeBoard(false);
+    } else {
+      setTimeout(() => {
+        firstCard.callback(true);
+        secondCard.callback(true);
+        setFirstCard(null);
+        setSecondCard(null);
+        setFreezeBoard(false);
+      }, 2000);
+    }
+  };
+
+  return (
+    <section id="board">
+      {cards.map((i, idx) => {
+        return (
+          <Card
+            onClick={handleCardClick}
+            boardIsFreeze={freezeBoard}
+            key={idx}
+            image={i}
+            order={orders[idx]}
+          />
+        );
+      })}
+    </section>
+  );
 }
